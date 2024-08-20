0704957548c883e913cda27f66372005b3510334
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-08-19 22:43:48 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *  # noqa: F403
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # noqa: F405
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
