46c82fb641d66155eecbf713f017c06faff4a119
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-08-19 23:16:03 -0400

    player (sans actually showing up) added

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pygame
-from constants import *  # noqa: F403
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # noqa: F405
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +19,11 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        delta = clock.tick(60)
+        dt = delta / 1000
+
+        player.draw(screen)
 
 
 if __name__ == "__main__":
